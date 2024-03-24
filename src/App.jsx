82d62e8b504108f501def313ed0f01bcbae1e213
noushin-nusrat <noushinnusrat21@gmail.com>
2024-03-24 21:12:46 +0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+
+import Blogs from "./pages/Blogs"
+import Bookmarks from "./pages/Bookmarks"
+import Home from "./pages/Home"
+
+
+
+function App() {
+  return (
+    <>
+      <Home></Home>
+      <Blogs></Blogs>
+      <Bookmarks></Bookmarks>
+    </>
+  )
+}
+
+export default App
---
+++ src/pages/Bookmarks.jsx
@@ -0,0 +1,11 @@
+
+
+const Bookmarks = () => {
+    return (
+        <div>
+            <h2>Bookmarks page</h2>
+        </div>
+    );
+};
+
+export default Bookmarks;
